019d0078d8fa7ac3e2cfdc28054dd59acc5a2364
matheusgevartosky <magevartosky@gmail.com>
2022-07-19 17:55:42 -0300

    Init UI

--- DevInCars.Project/Dapper/DataBase.cs
+++ DevInCars.Project/Dapper/DataBase.cs
@@ -1,5 +1,4 @@
-﻿
-using DevInCars.Project.Domain;
+﻿using DevInCars.Project.Domain;
 using DevInCars.Project.Domain.Base;
 using DevInCars.Project.Domain.Enum;
 
@@ -7,13 +6,13 @@ namespace DevInCars.Project.Dapper;
 
 public static class DataBase
 {
-    public static  List<VehicleFactoryBase> Data = new()
+    public static List<VehicleFactoryBase> Data = new()
     {
         new CarFactory(Guid.NewGuid(), new DateOnly(2013, 07, 23), "Punto", "xxx1234", 60.000,
             "Prata",
-            1.6, FuelType.Gasolina, VehicleStatus.Disponivel,1, VehicleType.Carro, 4, 4),
+            1.6, FuelType.Gasolina, VehicleStatus.Disponivel, 1, VehicleType.Carro, 4, 4),
         new CarFactory(Guid.NewGuid(), new DateOnly(2018, 07, 23), "Prisma", "ABC1234", 30.000,
             "Preto",
-            1.8, FuelType.Flex, VehicleStatus.Disponivel,2, VehicleType.Carro, 2, 2)
+            1.8, FuelType.Flex, VehicleStatus.Disponivel, 2, VehicleType.Carro, 2, 2)
     };
 }
---
+++ DevInCars.Project/Domain/UI/Menu.cs
@@ -0,0 +1,27 @@
+﻿using DevInCars.Project.Domain.Enum;
+
+namespace DevInCars.Project.Domain.UI;
+
+public static class Menu
+{
+    public static void MainMenu()
+    {
+        Console.WriteLine("Seja bem vindo! Selecione a operação que deseja efetuar " +
+                          "1 - Listar Veiculos " +
+                          "2 - Vender Veiculos" +
+                          "3 - Cadastrar Veiculos" 
+                          );
+        
+        var option = Console.ReadLine();
+        
+        switch (option)
+        {
+            case "1":
+                
+                break;
+                
+        }
+    }
+
+    
+}
